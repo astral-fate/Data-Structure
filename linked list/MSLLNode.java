fd83777c23e4239a6d1afcd1da2e15500694d802
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2023-06-01 22:35:16 +0300

    Update and rename MSLLNode to MSLLNode.java

---
+++ linked list/MSLLNode.java
@@ -0,0 +1,37 @@
+
+public class MSLLNode<T> {
+    private String key;
+    private SLL<T> list;
+    private MSLLNode<T> next;
+
+    public MSLLNode(String key, SLL<T> list, MSLLNode<T> next) {
+        this.key = key;
+        this.list = list;
+        this.next = next;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public SLL<T> getList() {
+        return list;
+    }
+
+    public void setList(SLL<T> list) {
+        this.list = list;
+    }
+
+    public MSLLNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(MSLLNode<T> next) {
+        this.next = next;
+    }
+}
+
