ebe6c3df5fefab986f50500e4fb28901aa009e8a
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2023-06-01 22:36:09 +0300

    Update SLLNode.java

--- linked list/SLLNode.java
+++ linked list/SLLNode.java
@@ -1,1 +1,26 @@
 
+public class SLLNode<T> {
+    private T data;
+    private SLLNode<T> next;
+
+    public SLLNode(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public SLLNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(SLLNode<T> next) {
+        this.next = next;
+    }
+}
