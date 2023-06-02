eb986dbe3af8d58e863d71b20a10e63ac4388c63
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2023-06-02 20:21:56 +0300

    Update SLL.java

--- linked list/SLL.java
+++ linked list/SLL.java
@@ -1,191 +1,87 @@
-public class SLL<T> {
-    private SLLNode<T> head;
+class SLL<T> {
+private SLLNode<T> head;
+private int size;
+public SLL() {
+    this.head = null;
+    this.size = 0;
+}
 
-    public SLL() {
-        this.head = null;
-    }
-
-    public SLLNode<T> getHead() {
-        return head;
-    }
-
-    public void addToTail(T data) {
-        SLLNode<T> newNode = new SLLNode<>(data);
-        if (head == null) {
-            head = newNode;
-        } else {
-            SLLNode<T> current = head;
-            while (current.getNext() != null) {
-                current = current.getNext();
-            }
-            current.setNext(newNode);
-        }
-    }
-
-    public boolean isInList(T data) {
-        SLLNode<T> current = head;
-        while (current != null) {
-            if (current.getData().equals(data)) {
-                return true;
-            }
-            current = current.getNext();
-        }
-        return false;
-    }
-
-    public void makeListEmpty() {
-        head = null;
-    }
-
-    public void deleteFromList(T data) {
-        if (head == null) {
-            throw new IllegalArgumentException("List is empty.");
-        }
-
-        if (head.getData().equals(data)) {
-            head = head.getNext();
-            return;
-        }
-
-        SLLNode<T> current = head;
-        while (current.getNext() != null) {
-            if (current.getNext().getData().equals(data)) {
-                current.setNext(current.getNext().getNext());
-                return;
-            }
-            current = current.getNext();
-        }
-
-        throw new IllegalArgumentException("Node with data does not exist.");
-    }
-
-    public void displayList() {
-        SLLNode<T> current = head;
-        while (current != null) {
-            System.out.print(current.getData() + " ");
-            current = current.getNext();
-        }
-        System.out.println();
-    }
-
-    public void makeEmpty() {
-        head = null;
-    }
-
-    public boolean isInSLL(String cityName) {
-        SLLNode<T> current = head;
-        while (current != null) {
-            if (current.getData() instanceof City) {
-                City city = (City) current.getData();
-                if (city.getCityName().equalsIgnoreCase(cityName)) {
-                    return true;
-                }
-            }
-            current = current.getNext();
-        }
-        return false;
-    }
-
-    public void deleteFromSLL(String cityName) {
-        if (head == null) {
-            throw new IllegalArgumentException("List is empty.");
-        }
-
-        if (head.getData() instanceof City) {
-            City city = (City) head.getData();
-            if (city.getCityName().equalsIgnoreCase(cityName)) {
-                head = head.getNext();
-                return;
-            }
-        }
-
-        SLLNode<T> current = head;
-        while (current.getNext() != null) {
-            if (current.getNext().getData() instanceof City) {
-                City city = (City) current.getNext().getData();
-                if (city.getCityName().equalsIgnoreCase(cityName)) {
-                    current.setNext(current.getNext().getNext());
-                    return;
-                }
-            }
-            current = current.getNext();
-        }
-
-        throw new IllegalArgumentException("Node with data does not exist.");
-    }
-
-    public void addToSLLPosition(T data, int position) {
-        SLLNode<T> newNode = new SLLNode<>(data);
-        if (position < 0) {
-            throw new IllegalArgumentException("Invalid position.");
-        }
-
-        if (position == 0) {
-            addToSLLHead(data);
-        } else {
-            SLLNode<T> current = head;
-            int currentPosition = 0;
-            while (current != null && currentPosition < position - 1) {
-                current = current.getNext();
-                currentPosition++;
-            }
-            if (current == null) {
-                throw new IllegalArgumentException("Position is out of bounds.");
-            }
-            newNode.setNext(current.getNext());
-            current.setNext(newNode);
-        }
-    }
-
-    public T findInSLL(String cityName) {
-        SLLNode<T> current = head;
-        while (current != null) {
-            if (current.getData() instanceof City) {
-                City city = (City) current.getData();
-                if (city.getCityName().equalsIgnoreCase(cityName)) {
-                    return current.getData();
-                }
-            }
-            current = current.getNext();
-        }
-        return null;
-    }
-    
-    
-
-    public void addToSLLHead(T data) {
-        SLLNode<T> newNode = new SLLNode<>(data);
-        newNode.setNext(head);
-        head = newNode;
-    }
-
-    public void addToSLLTail(T data) {
-        SLLNode<T> newNode = new SLLNode<>(data);
-        if (head == null) {
-            head = newNode;
-        } else {
-            SLLNode<T> current = head;
-            while (current.getNext() != null) {
-                current = current.getNext();
-            }
-            current.setNext(newNode);
-        }
-    }
+public SLLNode<T> getHead() {
+    return head;
+}
 
 public int getSize() {
-    int count = 0;
-    SLLNode<T> current = head;
-    while (current != null) {
-        count++;
-        current = current.getNext();
-    }
-    return count;
+    return size;
 }
-    Object getKey() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+public void addToSLLHead(T city) {
+    SLLNode<T> newNode = new SLLNode<>(city);
+    newNode.setNext(head);
+    head = newNode;
+    size++;
+}
+
+public void addToSLLTail(T city) {
+    SLLNode<T> newNode = new SLLNode<>(city);
+    if (head == null) {
+        head = newNode;
+    } else {
+        SLLNode<T> current = head;
+        while (current.getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(newNode);
+    }
+    size++;
+}
+
+public void deleteFromSLL(String city) {
+    if (head == null) {
+        return;
     }
 
-    void incrementSize() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    if (head.getData().equals(city)) {
+        head = head.getNext();
+        size--;
+        return;
     }
+
+    SLLNode<T> current = head;
+    while (current.getNext() != null) {
+        if (current.getNext().getData().equals(city)) {
+            current.setNext(current.getNext().getNext());
+            size--;
+            return;
+        }
+        current = current.getNext();
+    }
+}
+
+public boolean isInSLL(String city) {
+    SLLNode<T> current = head;
+    while (current != null) {
+        if (current.getData().equals(city)) {
+            return true;
+        }
+        current = current.getNext();
+    }
+    return false;
+}
+
+public void makeEmpty() {
+    head = null;
+    size = 0;
+}
+
+public void displayList() {
+    SLLNode<T> current = head;
+    while (current != null) {
+        System.out.println(current.getData());
+        current = current.getNext();
+    }
+}
+
+public void incrementSize() {
+    size++;
+}
 }
