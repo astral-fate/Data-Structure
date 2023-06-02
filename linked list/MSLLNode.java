f9b1f311eb93b2a7b15adbc177e13f938b14b6ab
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2023-06-02 20:22:10 +0300

    Update MSLLNode.java

--- linked list/MSLLNode.java
+++ linked list/MSLLNode.java
@@ -1,31 +1,27 @@
-public class MSLLNode<T> {
-    private String key;
-    private SLL<T> list;
-    private MSLLNode<T> next;
+class MSLLNode<T> {
+private String key;
+private SLL<T> list;
+private MSLLNode<T> next;
+public MSLLNode(String key, SLL<T> list, MSLLNode<T> next) {
+    this.key = key;
+    this.list = list;
+    this.next = next;
+}
 
-    public MSLLNode(String key, SLL<T> list, MSLLNode<T> next) {
-        this.key = key;
-        this.list = list;
-        this.next = next;
-    }
+public String getKey() {
+    return key;
+}
 
-    public String getKey() {
-        return key;
-    }
+public SLL<T> getList() {
+    return list;
+}
 
-    public SLL<T> getList() {
-        return list;
-    }
+public MSLLNode<T> getNext() {
+    return next;
+}
 
-    public void setList(SLL<T> list) {
-        this.list = list;
-    }
+public void setNext(MSLLNode<T> next) {
+    this.next = next;
+}
 
-    public MSLLNode<T> getNext() {
-        return next;
-    }
-
-    public void setNext(MSLLNode<T> next) {
-        this.next = next;
-    }
 }
