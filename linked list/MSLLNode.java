de93abb78cfe02fd0b81377f1293c9312fe1f687
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2023-06-03 13:46:48 +0300

    Update MSLLNode.java

--- linked list/MSLLNode.java
+++ linked list/MSLLNode.java
@@ -1,12 +1,25 @@
 class MSLLNode<T> {
 private String key;
 private SLL<T> list;
+private double latitude;
+    private double longitude;
 private MSLLNode<T> next;
 public MSLLNode(String key, SLL<T> list, MSLLNode<T> next) {
     this.key = key;
     this.list = list;
     this.next = next;
 }
+
+  
+    
+   
+
+    public MSLLNode(String key, SLL<T> list, double latitude, double longitude) {
+        this.key = key;
+        this.list = list;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
 public String getKey() {
     return key;
