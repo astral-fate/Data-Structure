574f9f8882d0d66ed603b49b38c0c6affc9d5af1
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2023-06-03 20:50:21 +0300

    Update MSLLNode.java

--- linked list/MSLLNode.java
+++ linked list/MSLLNode.java
@@ -1,40 +1,48 @@
 class MSLLNode<T> {
-private String key;
-private SLL<T> list;
-private double latitude;
+    private String key;
+    private SLL<T> list;
+    private double latitude;
     private double longitude;
-private MSLLNode<T> next;
-public MSLLNode(String key, SLL<T> list, MSLLNode<T> next) {
-    this.key = key;
-    this.list = list;
-    this.next = next;
-}
+    private MSLLNode<T> next;
 
-  
-    
-   
-
-    public MSLLNode(String key, SLL<T> list, double latitude, double longitude) {
+    public MSLLNode(String key, SLL<T> list, MSLLNode<T> next) {
         this.key = key;
         this.list = list;
+        this.next = next;
+    }
+
+    public MSLLNode(String key, double latitude, double longitude) {
+        this.key = key;
+        this.list = new SLL<>();
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-public String getKey() {
-    return key;
-}
+    public String getKey() {
+        return key;
+    }
 
-public SLL<T> getList() {
-    return list;
-}
+    public SLL<T> getList() {
+        return list;
+    }
 
-public MSLLNode<T> getNext() {
-    return next;
-}
+    public void setList(SLL<T> list) {
+        this.list = list;
+    }
 
-public void setNext(MSLLNode<T> next) {
-    this.next = next;
-}
+    public double getLatitude() {
+        return latitude;
+    }
 
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public MSLLNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(MSLLNode<T> next) {
+        this.next = next;
+    }
 }
