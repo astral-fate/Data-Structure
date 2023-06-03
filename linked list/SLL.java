f2d86d31b352cb087bb5c69fa7e5e98cdbedccf9
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2023-06-03 13:46:25 +0300

    Update SLL.java

--- linked list/SLL.java
+++ linked list/SLL.java
@@ -1,87 +1,129 @@
-class SLL<T> {
-private SLLNode<T> head;
-private int size;
-public SLL() {
-    this.head = null;
-    this.size = 0;
-}
+public class SLL<T> {
+    private SLLNode<T> head;
+    private int size;
 
-public SLLNode<T> getHead() {
-    return head;
-}
+    public SLL() {
+        this.head = null;
+        this.size = 0;
+    }
 
-public int getSize() {
-    return size;
-}
+    public SLLNode<T> getHead() {
+        return head;
+    }
 
-public void addToSLLHead(T city) {
-    SLLNode<T> newNode = new SLLNode<>(city);
-    newNode.setNext(head);
-    head = newNode;
-    size++;
-}
+    public int getSize() {
+        return size;
+    }
 
-public void addToSLLTail(T city) {
-    SLLNode<T> newNode = new SLLNode<>(city);
-    if (head == null) {
+    public void addToSLLHead(T city, double latitude, double longitude) {
+        SLLNode<T> newNode = new SLLNode<>(city, latitude, longitude);
+        newNode.setNext(head);
         head = newNode;
-    } else {
-        SLLNode<T> current = head;
-        while (current.getNext() != null) {
-            current = current.getNext();
-        }
-        current.setNext(newNode);
-    }
-    size++;
-}
-
-public void deleteFromSLL(String city) {
-    if (head == null) {
-        return;
+        incrementSize();
     }
 
-    if (head.getData().equals(city)) {
-        head = head.getNext();
-        size--;
-        return;
-    }
-
-    SLLNode<T> current = head;
-    while (current.getNext() != null) {
-        if (current.getNext().getData().equals(city)) {
-            current.setNext(current.getNext().getNext());
-            size--;
+    public void addToSLLPosition(T city, double latitude, double longitude, int position) {
+        if (position < 1 || position > size + 1) {
+            System.out.println("Invalid position. Position should be between 1 and " + (size + 1));
             return;
         }
-        current = current.getNext();
-    }
-}
 
-public boolean isInSLL(String city) {
-    SLLNode<T> current = head;
-    while (current != null) {
-        if (current.getData().equals(city)) {
-            return true;
+        SLLNode<T> newNode = new SLLNode<>(city, latitude, longitude);
+
+        if (position == 1) {
+            newNode.setNext(head);
+            head = newNode;
+        } else {
+            SLLNode<T> prev = head;
+            for (int i = 1; i < position - 1; i++) {
+                prev = prev.getNext();
+            }
+            newNode.setNext(prev.getNext());
+            prev.setNext(newNode);
         }
-        current = current.getNext();
+        incrementSize();
     }
-    return false;
-}
 
-public void makeEmpty() {
-    head = null;
-    size = 0;
-}
+    public void deleteFromSLL(String city) {
+        if (head == null) {
+            return;
+        }
 
-public void displayList() {
-    SLLNode<T> current = head;
-    while (current != null) {
-        System.out.println(current.getData());
-        current = current.getNext();
+        if (head.getCity().equals(city)) {
+            head = head.getNext();
+            decrementSize();
+            return;
+        }
+
+        SLLNode<T> current = head;
+        while (current.getNext() != null) {
+            if (current.getNext().getCity().equals(city)) {
+                current.setNext(current.getNext().getNext());
+                decrementSize();
+                return;
+            }
+            current = current.getNext();
+        }
     }
-}
 
-public void incrementSize() {
-    size++;
-}
+    public boolean isInSLL(String city) {
+        SLLNode<T> current = head;
+        while (current != null) {
+            if (current.getCity().equals(city)) {
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
+    public SLLNode<T> findSLLNode(String city) {
+        SLLNode<T> current = head;
+        while (current != null) {
+            if (current.getCity().equals(city)) {
+                return current;
+            }
+            current = current.getNext();
+        }
+        return null;
+    }
+
+    public T getElement(SLLNode<T> node) {
+        return node.getElement();
+    }
+
+    public void makeEmpty() {
+        head = null;
+        size = 0;
+    }
+
+    public void displayList() {
+        SLLNode<T> current = head;
+        while (current != null) {
+            System.out.println(current.getCity() + " (" + current.getLatitude() + ", " + current.getLongitude() + ")");
+            current = current.getNext();
+        }
+    }
+
+    void incrementSize() {
+        size++;
+    }
+
+    private void decrementSize() {
+        size--;
+    }
+
+    public void addToSLLTail(T element, double latitude, double longitude) {
+        SLLNode<T> newNode = new SLLNode<>(element, latitude, longitude);
+        if (head == null) {
+            head = newNode;
+        } else {
+            SLLNode<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+        size++;
+    }
 }
