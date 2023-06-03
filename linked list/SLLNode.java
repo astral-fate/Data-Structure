bccdef306139857c89ee078bf926ba63dadf517a
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2023-06-03 13:46:05 +0300

    Update SLLNode.java

--- linked list/SLLNode.java
+++ linked list/SLLNode.java
@@ -1,10 +1,13 @@
-
 public class SLLNode<T> {
     private T data;
+    private double latitude;
+    private double longitude;
     private SLLNode<T> next;
 
-    public SLLNode(T data) {
+    public SLLNode(T data, double latitude, double longitude) {
         this.data = data;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.next = null;
     }
 
@@ -16,6 +19,22 @@ public class SLLNode<T> {
         this.data = data;
     }
 
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
     public SLLNode<T> getNext() {
         return next;
     }
@@ -23,4 +42,44 @@ public class SLLNode<T> {
     public void setNext(SLLNode<T> next) {
         this.next = next;
     }
+
+    public String getCity() {
+        return data.toString();
+    }
+    
+    
+  
+    private T element;
+ 
+
+
+
+    public T getLelemnt() {
+        return element;
+    }
+   
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+   
+   
+
+    public SLLNode<T> findNode(T target) {
+        SLLNode<T> currentNode = this;
+        while (currentNode != null) {
+            if (currentNode.getElement().equals(target)) {
+                return currentNode;
+            }
+            currentNode = currentNode.getNext();
+        }
+        return null;
+    }
 }
+
+
